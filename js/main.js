7afd9706610bc5057eb10a85fefeee602711f620
CO0Ki3 <ehrrhgus0324@gmail.com>
2017-08-15 21:51:04 +0900

    AppJam Page Source, But JS was nat completed.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#three").click(function() {
+    location.href="private.html"
+  })
+  $("#eight").click(function() {
+    location.href="https://www.facebook.com/smarteenappclub/?fref=ts";
+  });
+})
---
+++ js/what.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $(".go-main-page-img").click(function() {
+    location.href="file:///home/co0ki3/%EB%B0%94%ED%83%95%ED%99%94%EB%A9%B4/appjam/main.html";
+  });
+})
